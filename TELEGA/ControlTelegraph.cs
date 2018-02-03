78359819843de582462f42462aef228e7c397c83
Ande5 <33572132+Ande5@users.noreply.github.com>
2018-02-03 22:14:34 +0300

    Добавление класса ControlTelegraph

---
+++ TELEGA/ControlTelegraph.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegraph;
+using Telegraph.Net;
+using Telegraph.Net.Models;
+
+namespace TELEGA
+{
+    class ControlTelegraph
+    {
+        private TelegraphClient m_client;
+        private ITokenClient m_tokenClient;
+        private List<NodeElement> m_node_element = new List<NodeElement>();
+
+        public ControlTelegraph()
+        {
+            m_client = new TelegraphClient();
+            m_tokenClient = m_client.GetTokenClient("40b9813e6e0303023473d29a70eee7fd37342fbecabe0b7734bd0229d98a");
+        }
+
+        public async void CreatePage()
+        {
+            Page newPage = await m_tokenClient.CreatePageAsync(
+              "Sample Page",
+              content: new List<NodeElement> {
+                    new NodeElement("p", null, "Hello, world!")
+               }.ToArray(),
+              returnContent: true
+            );
+        }
+
+        public void AddListNodeElement(List<CheckProduct> products)
+        {
+            foreach(var product in products)
+            {
+                m_node_element.Add(
+                    new NodeElement("ol", null,
+                       new NodeElement("b", null, "Продукт: "),
+                       new NodeElement("li", null, product.Product_Name, new NodeElement("b", null, " Сумма: "),
+                       new NodeElement("text", null, product.Product_Sum + "руб."),
+                       new NodeElement("b", null, " Магазин: "),
+                       new NodeElement("text", null, product.Store_Name))
+                       ));
+            }
+        }
+        public void AddListNodeElementNew(List<CheckProduct> products)
+        {
+            List<NodeElement> elem = new List<NodeElement>();
+            foreach (var product in products)
+            {
+                elem.Add(new NodeElement("b", null, "Продукт: "));
+                elem.Add(new NodeElement("li", null, product.Product_Name, new NodeElement("b", null, " Сумма: "),
+                         new NodeElement("text", null, product.Product_Sum + "руб."), new NodeElement("b", null, " Магазин: "),
+                         new NodeElement("text", null, product.Store_Name)));
+            }
+            m_node_element.Add(new NodeElement("ol", null, elem.ToArray()));
+        }
+        public async void EditPage()
+        {
+            Page editedPage = await m_tokenClient.EditPageAsync(
+             "Sample-Page1-02-03",
+             "Информация о чекe",
+             content: m_node_element.ToArray(), returnContent: true
+             );
+        }
+
+    }
+}
--- TELEGA/Program.cs
+++ TELEGA/Program.cs
@@ -126,10 +126,27 @@ namespace Telegram.Bot.Examples.Echo
                         }
                         break;
                     }
+                case "/Telegraph":
+                    {
+                        string product = message.Text.Remove(0, message.Text.IndexOf(' ') + 1);
+                        ControlTelegraph control_telegraph = new ControlTelegraph();
+                         Data_Analysis data_analysis = new Data_Analysis(my_sql_control);
+                         control_telegraph.AddListNodeElementNew(data_analysis.Parser_Check(product));
+                         control_telegraph.EditPage();
+                         await Bot.SendTextMessageAsync(message.Chat.Id, "http://telegra.ph//Sample-Page1-02-03");
+                         //http://telegra.ph//Sample-Page1-02-03-2
+                        //foreach (var check in data_analysis.Parser_Check(product))
+                        //{
+                        //    await Bot.SendTextMessageAsync(message.Chat.Id, string.Format("{0}   {1} руб.   {2}", check.Product_Name,
+                        //        check.Product_Sum, check.Store_Name));
+                        //}
+                        break;
+                    }
                 default:
                     const string usage = @"Usage:
 /info   - Информация о продукте
-/Product - Инфо по продукту";
+/Product - Инфо по продукту
+/Telegraph - Вывести информацию телеграф";
 
                     await Bot.SendTextMessageAsync(message.Chat.Id,usage, replyMarkup: new ReplyKeyboardRemove());
                     break;
