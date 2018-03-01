07784a456f9241e14315fdd30126e2a314f35a15
Ande5 <33572132+Ande5@users.noreply.github.com>
2018-03-01 22:56:02 +0300

    Добавление класса ProductStandart

---
+++ TELEGA/ProductStandart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TELEGA
+{
+    class ProductStandart
+    {
+        private string Product_name;
+        private string Product_brand;
+        private string Refinement;
+
+        private ProductStandart(string product_name, string product_brand, string refinement)
+        {
+            Product_name = product_name;
+            Product_brand = Product_brand;
+            Refinement = refinement;
+        }
+    }
+}
